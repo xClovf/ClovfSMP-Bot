3f93dd1d788fa5bd4ababac3018785500afc10fd
xClovf <finn.amedick@gmx.de>
2026-06-13 22:14:32 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "ClovfSMP.de🍀",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#00FF00", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
