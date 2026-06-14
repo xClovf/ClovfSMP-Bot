ec8eb5cfc80a8289fb632c4e10ec2d8796404ade
xClovf <finn.amedick@gmx.de>
2026-06-14 14:32:23 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "xClovf ist Cool",
+        name: "ClovfSMP.de🍀",
         // Activity type number (0 = Playing).
         type: 0, 
       },
